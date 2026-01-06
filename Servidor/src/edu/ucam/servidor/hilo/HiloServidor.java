2df5a10a5d200cd29b0637285fb2fd2b73d51d76
Agata-gp <agambin7@alu.ucam.edu>
2026-01-06 19:50:24 +0100

    Update HiloServidor.java

--- Servidor/src/edu/ucam/servidor/hilo/HiloServidor.java
+++ Servidor/src/edu/ucam/servidor/hilo/HiloServidor.java
@@ -1,45 +1,59 @@
 package edu.ucam.servidor.hilo;
 
 import java.io.*;
-import java.net.*;
+import java.net.Socket;
+import java.util.Hashtable;
+
+import edu.ucam.servidor.comando.*;
 
 public class HiloServidor implements Runnable {
 
     private Socket socket;
-    private PrintWriter out;
     private BufferedReader in;
+    private PrintWriter out;
 
-    private ServidorRepository repository = new ServidorRepository();
+    private Hashtable<String, Class<? extends Comando>> comandos = new Hashtable<>();
 
     public HiloServidor(Socket socket) {
         this.socket = socket;
+
+        // Registrar comandos
+        comandos.put("USER", ComandoUSER.class);
+        comandos.put("PASS", ComandoPASS.class);
+        comandos.put("EXIT", ComandoEXIT.class);
+        comandos.put("ADDTIT", ComandoADDTIT.class);
+        comandos.put("GETTIT", ComandoGETTIT.class);
     }
 
     @Override
     public void run() {
 
         try {
-            in = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream())
-            );
-            out = new PrintWriter(
-                    socket.getOutputStream(),
-                    true
-            );
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
 
             String linea;
             while ((linea = in.readLine()) != null) {
-                repository.ejecutar(linea, socket, in, out);
+
+                String[] partes = linea.split(" ");
+                String comandoStr = partes[1];
+
+                Class<? extends Comando> clase = comandos.get(comandoStr);
+
+                if (clase == null) {
+                    out.println("FAILED " + partes[0] + " 400 Comando desconocido");
+                    continue;
+                }
+
+                Comando comando = clase
+                        .getConstructor(Socket.class, BufferedReader.class, PrintWriter.class, String[].class)
+                        .newInstance(socket, in, out, partes);
+
+                comando.ejecutar();
             }
 
         } catch (Exception e) {
             System.out.println("Cliente desconectado");
-        } finally {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
