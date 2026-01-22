c97ea19798f2eab0db02c56f870a5a8bb1010d73
Agata-gp <agambin7@alu.ucam.edu>
2026-01-22 21:01:36 +0100

    Update DataRepository.java

--- Servidor/src/edu/ucam/servidor/data/DataRepository.java
+++ Servidor/src/edu/ucam/servidor/data/DataRepository.java
@@ -1,6 +1,5 @@
 package edu.ucam.servidor.data;
 
-import java.util.ArrayList;
 import java.util.Hashtable;
 import java.lang.reflect.Method;
 
@@ -8,60 +7,24 @@ public class DataRepository<T> {
 
     private Hashtable<String, T> datos = new Hashtable<>();
 
-    
-    
-    public void add(T obj) {
-        String id = obtenerId(obj);
-        
-        if (id != null) {
-            datos.put(id, obj);
-        } else {
-            System.out.println("Error: No se pudo obtener el ID del objeto.");
+    private String getId(T obj) {
+        try {
+            Method m = obj.getClass().getMethod("getId");
+            return (String) m.invoke(obj);
+        } catch (Exception e) {
+            throw new RuntimeException("El objeto no tiene método getId()");
         }
     }
 
-    
-    
+    public void add(T obj) {
+        datos.put(getId(obj), obj);
+    }
+
     public T get(String id) {
         return datos.get(id);
     }
 
-    
     public boolean existe(String id) {
         return datos.containsKey(id);
     }
-
-  
-    private String obtenerId(T obj) {
-    	
-        try {
-
-        	Method metodoGetId = obj.getClass().getMethod("getId");
-            
-            Object resultado = metodoGetId.invoke(obj);
-            
-            return (String) resultado;
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-    
-    public void remove(String id) {
-        datos.remove(id);
-    }
-    
-    public ArrayList<T> getTodos() {
-        return new ArrayList<>(datos.values());
-    }
-    
-    
-    
-    public int getSize() { //Para contar usuarios. 
-        return datos.size();
-    }
-    
-    
-    
 }
